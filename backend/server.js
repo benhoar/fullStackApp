d42076f3e41b250efe91c43203fb3c61fd65f497
benhoar <bbhoar@gmail.com>
2023-01-16 12:19:40 -0800

    attempting fix to server

--- backend/server.js
+++ backend/server.js
@@ -23,11 +23,11 @@ app.use('/api/cuisines', require('./routes/cuisineRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 
 if (process.env.NODE_ENV === 'production') {
-   app.use(express.static(path.join(__dirname, '../frontend/build')));
+   app.use(express.static(path.join('../frontend/build')));
  
    app.get('*', (req, res) =>
      res.sendFile(
-       path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+       path.resolve('./', 'frontend', 'build', 'index.html')
      )
    );
 } else {
