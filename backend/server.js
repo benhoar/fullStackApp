6e554e71462077592afa7e62859467a740a6247c
benhoar <bbhoar@gmail.com>
2023-01-16 12:26:03 -0800

    attempting fix to server

--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,9 @@ app.use('/api/users', require('./routes/userRoutes'))
 
 if (process.env.NODE_ENV === 'production') {
    app.use(express.static(path.join(__dirname, '../frontend/build')));
- 
-   app.get('*', (req, res) =>
+   app.get('*', (req, res) => 
      res.sendFile(
-       path.resolve('./', 'frontend', 'build', 'index.html')
+       path.resolve('../', 'frontend', 'build', 'index.html')
      )
    );
 } else {
