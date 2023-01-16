c3d4a01062795ee6f6af62a0a84bad19584318db
benhoar <bbhoar@gmail.com>
2023-01-16 11:23:50 -0800

    checking environment variables2

--- backend/server.js
+++ backend/server.js
@@ -36,11 +36,11 @@ if (process.env.NODE_ENV === 'production') {
 app.use(errorHandler)
 
 app.listen(port, () => {
-   console.log(`Server started on port ${port}`)
    console.log(process.env.NODE_ENV)
    console.log(process.env.JWT_SECRET)
    console.log(process.env.REGISTER_SECRET)
    console.log(process.env.MONGO_URI)
+   console.log(`Server started on port ${port}`)
 })
 
 /*
