dcd007184bc4e3a95b6fca14e71246db8ae01a76
benhoar <bbhoar@gmail.com>
2023-01-19 18:46:53 -0800

    much ~40% smaller background image to aid in loading

--- frontend/src/components/map/Map.jsx
+++ frontend/src/components/map/Map.jsx
@@ -6,7 +6,7 @@ const Map = ({ mapState, mapDispatch, setSelected, hide }) => {
 
   return (
     <div className="wrapper">
-      <img src="images/waterlessmap.png" className="map" alt=""/>
+      <img src="images/waterlessworld2x.png" className="map" alt=""/>
       {Object.entries(countries).map((country) => {
         return <Marker key={country[0]} country={country} mapState={mapState} mapDispatch={mapDispatch} setSelected={setSelected} hide={hide}/>
       })}
